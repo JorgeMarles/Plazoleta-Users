872cf352ad877f7b13788c65270938275505a00d
Jorge Marles <jamarlesf@gmail.com>
2026-06-07 21:10:17 -0500

    chore(domain): User Service Port

---
+++ src/main/java/com/jamarlesf/plazoletausers/domain/api/IUserServicePort.java
@@ -0,0 +1,10 @@
+package com.jamarlesf.plazoletausers.domain.api;
+
+import com.jamarlesf.plazoletausers.domain.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IUserServicePort {
+    void save(User user);
+}
