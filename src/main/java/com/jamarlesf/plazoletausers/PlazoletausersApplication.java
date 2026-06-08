24b1ad24545833adac133c117ab9e26714eeeb6b
Jorge Marles <jamarlesf@gmail.com>
2026-06-07 19:32:40 -0500

    first commit

---
+++ src/main/java/com/jamarlesf/plazoletausers/PlazoletausersApplication.java
@@ -0,0 +1,13 @@
+package com.jamarlesf.plazoletausers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlazoletausersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlazoletausersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jamarlesf/plazoletausers/PlazoletausersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamarlesf.plazoletausers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlazoletausersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
