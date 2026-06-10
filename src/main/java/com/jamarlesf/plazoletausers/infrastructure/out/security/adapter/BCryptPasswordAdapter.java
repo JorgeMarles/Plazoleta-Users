982526c63504d216fd23cab7c68d54fc415eae04
Jorge Marles <jamarlesf@gmail.com>
2026-06-09 20:09:37 -0500

    feat(infra): Implement BCryptPasswordAdapter

---
+++ src/main/java/com/jamarlesf/plazoletausers/infrastructure/out/security/adapter/BCryptPasswordAdapter.java
@@ -0,0 +1,23 @@
+package com.jamarlesf.plazoletausers.infrastructure.out.security.adapter;
+
+import com.jamarlesf.plazoletausers.domain.spi.IEncryptionPort;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BCryptPasswordAdapter implements IEncryptionPort {
+
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public BCryptPasswordAdapter() {
+        this.passwordEncoder = new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public String encryptPassword(String password) {
+        if (password == null || password.isBlank()) {
+            throw new IllegalArgumentException("Password cannot be null or empty");
+        }
+        return passwordEncoder.encode(password);
+    }
+}
