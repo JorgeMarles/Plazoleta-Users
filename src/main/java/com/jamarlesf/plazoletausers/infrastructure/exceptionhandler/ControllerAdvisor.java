04d30f9d614d386529f2496b138593a2754935d8
Jorge Marles <jamarlesf@gmail.com>
2026-06-10 20:01:48 -0500

    fix(infra): DomainException handling in ControllerAdvisor

--- src/main/java/com/jamarlesf/plazoletausers/infrastructure/exceptionhandler/ControllerAdvisor.java
+++ src/main/java/com/jamarlesf/plazoletausers/infrastructure/exceptionhandler/ControllerAdvisor.java
@@ -1,7 +1,7 @@
 package com.jamarlesf.plazoletausers.infrastructure.exceptionhandler;
 
+import com.jamarlesf.plazoletausers.domain.exception.DomainException;
 import com.jamarlesf.plazoletausers.domain.exception.RoleNotFoundException;
-import com.jamarlesf.plazoletausers.domain.exception.UserEmailAlreadyExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,14 +14,14 @@ public class ControllerAdvisor {
 
     private static final String MESSAGE = "message";
 
-    @ExceptionHandler(UserEmailAlreadyExistsException.class)
-    public ResponseEntity<Map<String, String>> handleUserEmailAlreadyExistsException(UserEmailAlreadyExistsException ex) {
-        return ResponseEntity.badRequest().body(Map.of(MESSAGE, ex.getMessage()));
-    }
-
     @ExceptionHandler(RoleNotFoundException.class)
     public ResponseEntity<Map<String, String>> handleRoleNotFoundException(RoleNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of(MESSAGE, ex.getMessage()));
+    }
+
+    @ExceptionHandler(DomainException.class)
+    public ResponseEntity<Map<String, String>> handleDomainException(DomainException ex) {
+        return ResponseEntity.badRequest().body(Map.of(MESSAGE, ex.getMessage()));
     }
 
 }
