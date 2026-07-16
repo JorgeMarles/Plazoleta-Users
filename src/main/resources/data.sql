f62d08d93989fba14f0acef7ca677bec5766923c
= <=>
2026-07-15 21:42:04 -0500

    feat(infra): add database seeding with roles and admin user initialization

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT IGNORE INTO roles (role_id, name, description) VALUES (1, 'ADMINISTRADOR', 'Administrador del Sistema');
+INSERT IGNORE INTO roles (role_id, name, description) VALUES (2, 'PROPIETARIO', 'Propietario de Restaurante');
+INSERT IGNORE INTO roles (role_id, name, description) VALUES (3, 'EMPLEADO', 'Empleado de Restaurante');
+INSERT IGNORE INTO roles (role_id, name, description) VALUES (4, 'CLIENTE', 'Cliente de la Plazoleta');
